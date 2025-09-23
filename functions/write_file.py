6e1e1cf371a93bb8978abaedf5e5942c9eabb9c2
ZafirChowdhury <zafirchowdhury69@gmail.com>
2025-09-23 14:31:24 +0600

    Add write_file

---
+++ functions/write_file.py
@@ -0,0 +1,22 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+
+    
+    # calculator/pkg/morelorem.txt
+    dir_path = "/".join(abs_file_path.split("/")[:-1])
+    try:
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+
+        with open(abs_file_path, "w") as file:
+                file.write(content)
+                return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        
+    except Exception as error:
+        return f"Exeption while writing to file: {error}"
