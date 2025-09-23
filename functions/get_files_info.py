d027ba4edc0168beeada8bce4a173e85b7c82eeb
ZafirChowdhury <zafirchowdhury69@gmail.com>
2025-09-23 11:55:02 +0600

    Refactor

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,24 +1,29 @@
 import os
 
 def get_files_info(working_directory, directory="."):
+    output_str = f"Result for {"current" if directory == "." else directory} directory:" + "\n"
+
     if directory.startswith("/"):
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        output_str += f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        return output_str
     
     if len(directory) >= 2:
         if directory[0] == "." and (directory[1] == "." or directory[1] == "/"):
-            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+            output_str += f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+            return output_str
     
     full_reletive_path = os.path.join(working_directory, directory)
     abs_path = os.path.abspath(full_reletive_path)
 
     if not os.path.isdir(abs_path):
-        return f'Error: "{directory}" is not a directory'
+        output_str += f'Error: "{directory}" is not a directory'
+        return output_str
     
     list_dir = os.listdir(abs_path)
     for file_name in list_dir:
         file_path = os.path.join(abs_path, file_name)
         file_size = os.path.getsize(file_path)
         is_dir = os.path.isdir(file_path)
-        print(f"- {file_name}: file_size={file_size} bytes, is_dir={is_dir}")
+        output_str += f"- {file_name}: file_size={file_size} bytes, is_dir={is_dir}" + "\n"
 
-    return ""
+    return output_str
